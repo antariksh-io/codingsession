1eff47542e42e01c40d2c75ef9fedc0e20e1f25e
Antariksh Chavhan <antarikshc.work@gmail.com>
2026-03-13 14:45:40 +0530

    second half

--- s4.py
+++ s4.py
@@ -67,7 +67,28 @@
 #  3 3 3
 
 # using nested for loop
-for i in range(1, 4):
-    for j in range(1, 4):
+
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(i, end=" ")
+#     print()
+
+
+# n = int (input("Enter the number of rows: "))
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print(chr(64+i), end=" ")
+#     print()
+
+# n = int (input("Enter the number of rows: "))
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print(n+1-i, end=" ")
+#     print()
+
+n = int (input("Enter the number of rows: "))
+for i in range(1, n+1):
+    for j in range(1, 1+i):
         print(i, end=" ")
     print()
